817bdfa268cf1bc320598c8e908eb3b9295da773
vitaliy <vitaliy.skoryy@googlemail.com>
2022-07-18 20:12:01 +0200

    base line

---
+++ Email.java
@@ -0,0 +1,25 @@
+public class Email {
+private String firstName;
+private String lastName;
+private String password;
+private String department;
+private int mailboxCapacity;
+private String alternateEmail;
+
+// Constructor to recieve the first name, last name.
+public Email(String firstName, String lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  System.out.println("Email created for " + firstName + " " + lastName);
+}
+// Ask for the department.
+
+// Generate a random password
+
+// Set mailbox capacity
+
+// Set the alternate email address
+
+// Change the password
+
+}
